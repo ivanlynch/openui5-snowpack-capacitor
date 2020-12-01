42692e46deae1087c4fba833a33ab5dd98af2a42
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-12-01 19:38:45 -0300

    Changed default url to 0.0.0.0 Deleted default styles Changed title name of the project Added image to welcome screen of the projecDeleted default styles Changed title name of the project Added image to welcome screen of the projectt

--- www/controller/Home.controller.js
+++ www/controller/Home.controller.js
@@ -1,11 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ctx.controller.Home", {
 		onInit : function () {
 
+			const oImage = sap.ui.require.toUrl("ctx/img/img.jpeg");
+
+			this.getView().setModel(new JSONModel({
+				image: oImage
+			}));
 		}
 	});
 });
